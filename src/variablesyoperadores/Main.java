ab0b2eac2926408793f5ec5126a490478e07d4f9
JoseOsorio4 <josorioo4@miumg.edu.gt>
2026-02-20 20:52:06 -0600

    Cambio final
    Codigo correjido

--- src/variablesyoperadores/Main.java
+++ src/variablesyoperadores/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-      }
+        Scanner sc = new Scanner(System.in);
 
         //  1 Variables 
         System.out.println("DATOS USUARIO DE ESTUDIANTE");
